b1213e8dd11dca30ac13e67fae6df7009df1d33d
AliVasped200 <152574439+AndresVasped@users.noreply.github.com>
2024-04-03 11:51:29 -0500

    Update former.js

--- former.js
+++ former.js
@@ -1,8 +1,15 @@
-productos=document.getElementById('produs');
+let productos=document.getElementById('produs');
 function traerElementos()
 {
-    const valor = localStorage.getItem('clave');
-    console.log(valor);
+    //const valor = localStorage.getItem('');
+    for(let i=1;i<localStorage.length;i++)
+    {
+        const key=localStorage.key(i);//obtenemos la key
+        const valor=localStorage.getItem(key);//la pasamos
+        console.log(valor);
+        const nombre=document.createTextNode(valor);
+        productos.appendChild(nombre);
+    } 
 }
 
 window.onload=traerElementos;
