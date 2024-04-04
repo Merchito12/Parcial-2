283e7295d512b8fb9c073ccbf09d221843503954
Merchito12 <brandonmesa@unisabana.edu.co>
2024-04-04 14:32:55 -0500

    a

--- former.js
+++ former.js
@@ -1,56 +1,154 @@
 const productos=document.getElementById('produs');
 const form=document.getElementById('form');
+const favi = document.getElementById('favs');
+const eliminarfa = document.getElementById("eliminar")
+
+const resultado = document.getElementById('result');
+const enviar = document.getElementById('Enviar');
 const productosInput = document.getElementById('productosInput');
 const total=document.getElementById('TotalInput');
 const produs=[];
 const sheetsProdus=[];
-function traerElementos()
-{
-    let key;
-    let valor;
-    let objeto;
-    for(let i=1;i<localStorage.length;i++)
-    {
-        key=localStorage.key(i);//obtenemos la key
-        valor=localStorage.getItem(key);//la pasamos
-        objeto=JSON.parse(valor);//parseamos el elemento a JSON
-    } 
-    console.log(objeto);
-    const nameValue=objeto[0].Name;//obtenemos el nombre y lo colocamos en el 
-    const totalValue=objeto[objeto.length -1].Total;
-    console.log(totalValue);
-    produs.push(nameValue);//lo agregamos al array donde mostraremos en el documento
-        
-    const nombre=document.createTextNode(produs);
-    console.log(nombre.textContent);
-    productos.appendChild(nombre);
+var products ="";
+function traerElementos(opcion){
+  
+  if (opcion) {
+    products += opcion.Nombre + ",  ";
+    productos.innerHTML = products;
+  } else {
+    products = "";
+  }
+}
+function mostrarfav() {
 
-    sheetsProdus.push(valor);//este almacenara todos los elementos que el uusario pidio
-    //almacenaremos todos los pedidos del usuario al sheets
-    const toShteets=document.createTextNode(sheetsProdus);
-    const content=toShteets.textContent;
-    productosInput.value = content;
+  favi.innerHTML = "";
+  const keys = Object.keys(localStorage);
 
-    const toShteetsTotal=document.createTextNode(totalValue);
-    const contentTotal=toShteetsTotal.textContent;
-    total.value = contentTotal;
+  fetch("https://script.google.com/macros/s/AKfycbz3EPAgZePOY21THYydS3_LH-G1ntkhneenTOSaq257QQzqevuoMXUUt6jV8EOB4JPv/exec")
 
+      .then(response => response.json())
+      .then(dataPre => {
+          
+          var data = dataPre.data;
+          
+          
+          for (let h = 0; h < keys.length; h++) {
+              let id = keys[h];
+              const opcion = data[id-1];
+         
+
+              const article = document.createElement('article');
+              article.classList.add('opcion');
+          
+              const idopcion = opcion.ID;
+              const imagenopcion = document.createElement('img');
+              imagenopcion.src = opcion.Imagen;
+             
+              article.appendChild(imagenopcion);
+          
+              const nombreOpcion = document.createElement('h2');
+              nombreOpcion.setAttribute('class', 'name');
+              nombreOpcion.textContent = opcion.Nombre;
+              article.appendChild(nombreOpcion);
+          
+              const precioOpcion = document.createElement('h2');
+              precioOpcion.setAttribute('class', 'precio');
+              precioOpcion.textContent = `Precio: $ ${opcion.Precio}`;
+              article.appendChild(precioOpcion);
+          
+             
+              favi.appendChild(article);
+                  
+              Total(opcion.Precio);
+              traerElementos(opcion);
+              
+          }
+          
+      });
 }
 
-function doPost()
-{
-    form.addEventListener("submit", function(e) {
-        e.preventDefault();
-        const data = new FormData(form);
-        const action = e.target.action;
-        fetch(action, {
-          method: 'POST',
-          body: data,
-        })
-        .then(() => {
-          alert("Success!");
-        })
-    });
-}
+function Total(precio) {
 
-window.onload=traerElementos(),doPost();
+  const preci = precio;
+  
+  let total = parseFloat(resultado.innerText);//obtenemos la info de resultado
+  total += parseFloat(preci);//sumamos
+  resultado.textContent = total.toFixed(2);//agregamos dos decimales
+
+}
+function eliminarf() {
+  if (window.confirm('¿Estás seguro de que deseas eliminar los favoritos?')) {
+      localStorage.clear();
+     /*  let resta = parseFloat(resultado.innerText);
+      resta -= parseFloat(precio);
+      resultado.textContent = resta.toFixed(2); */
+      mostrarfav();
+      resultado.textContent="0";
+  } else {
+      console.log("ok");
+  }
+  mostrarfav();
+}
+eliminarfa.addEventListener('click', function () {
+
+  eliminarf();
+  
+});
+document.getElementById("form").addEventListener("submit", function (event) {
+ event.preventDefault(); // Evita que el formulario se envíe de forma predeterminada
+
+   /*  if (Object.keys(listaJSON).length === 0) {
+      window.alert("No hay productos en el carrito");
+      return;
+
+    } */
+    // Obtén los valores de los campos del formulario
+    var nombre = document.getElementById("nombre").value;
+    var numero = document.getElementById("numero").value;
+    var direccion = document.getElementById("direccion").value;
+
+    // Crea un objeto FormData para enviar los datos del formulario
+    var formData = new FormData();
+    formData.append("nombre", nombre);
+    formData.append("numero", numero);
+    formData.append("direccion", direccion);
+    formData.append("productosPedido", products);
+    
+
+    var object = {};
+    formData.forEach((value, key) => (object[key] = value));
+    var json = JSON.stringify(object);
+
+   
+    // Realiza la solicitud POST mediante fetch
+    fetch(
+      "https://script.google.com/macros/s/AKfycby8LoRQd_XmCsIRMngcTLwUCFeA9Eo7xhhMJCoYAwEeskKbuW2-7_B_bvf3_Xzxv770/exec",
+      {
+        method: "POST",
+        body: formData,
+      }
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        /* ocultarLoading();
+        ocultarBloqueoPantalla(); */
+        alert("¡Compra realizada con éxito!");
+        console.log(data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        alert("Error al enviar el formulario. Por favor, inténtalo de nuevo.");
+      });
+
+   /*  limpiarCarrito();
+    limpiarFormulario();
+    actualizarTotal(0); */
+  });
+
+
+
+
+
+
+
+window.onload= mostrarfav(),traerElementos();
--- main.js
+++ main.js
@@ -1,220 +1,152 @@
 const botonrandom = document.querySelector('.random');
-
 const id_random = document.querySelector(".id-producto_random");
 const imagen_random = document.getElementById("imagen_random");
 const nombre_random = document.querySelector(".nombre-producto_random");
 const categoria_random = document.querySelector(".categoria_random");
 const ingredientes_random = document.querySelector(".ingredientes_random");
 const instrucciones_random = document.querySelector(".instrucciones_random");
-
 const buscarb = document.getElementById('buscarb');
-
-
 const favi = document.getElementById('favs');
 const eliminarfa = document.getElementById("eliminar")
-
-const resultado=document.getElementById('result');
-const enviar=document.getElementById('Enviar');
+const resultado = document.getElementById('result');
+const enviar = document.getElementById('Enviar');
 const foodInput = document.getElementById('input');
 const foodList = document.getElementById('foodlList');
-const entradas=document.getElementById('entrad');
-const pedidos=[];
+const entradas = document.getElementById('entrad');
 
 
-/*
-buscarb.addEventListener('click', function () {
-    buscarc();
-});
-*/
 function buscarc() {
-    foodList.innerHTML= "";
+    foodList.innerHTML = "";
     fetch("https://script.google.com/macros/s/AKfycbz3EPAgZePOY21THYydS3_LH-G1ntkhneenTOSaq257QQzqevuoMXUUt6jV8EOB4JPv/exec")
         .then(response => response.json())
         .then(dataPre => {
             let data = dataPre.data;//traigo la info de json
-            /* if (data.data) { */
             console.log(data);
-            //const idCoctel1 =data[0].ID;
-            //console.log(idCoctel1)
-            //console.log(data.data[0].Nombre)
-                for(let n = 0; n <10; n++)
-                {
-                    if (data[n].ID != null) 
-                    {
-                        const opcion = data[n];
-                        elementos(opcion);
-                        /*
-                        eliminarfa.addEventListener('click', function () {
+            for (let n = 0; n < 10; n++) {
+                if (data[n].ID != null) {
+                    const opcion = data[n];
+                    elementos(opcion);
 
-                            eliminarf(opcion.Precio);
-                            
-                        
-                        });
-                        */
-                    } else {
-                        console.log("no encontrado")
-                    }
-
+                } else {
+                    console.log("no encontrado")
                 }
+            }
         });
-
-
-
 }
-function guardarLocal(opcion) 
-{
+function guardarLocal(opcion) {
     favi.innerHTML = "";
     const idFood = opcion.ID;
-    const nombre = opcion.Nombre;    
-
-    const elemfavo={//objeto para guardar los elementos favoritos
-        Name:opcion.Nombre,
-        ID:opcion.ID,
-        Price:opcion.Precio,
-        Total:resultado.textContent
-        
-    }
-    pedidos.push(elemfavo);
-    console.log(pedidos);
+    const nombre = opcion.Nombre;
     localStorage.setItem(idFood, JSON.stringify(pedidos))
     alert(`Se ha agregado al carrito el cóctel: ${nombre}!`);
     let ids = localStorage.getItem(idFood)
-    //mostrarfav();
+    mostrarfav();
 }
-function mostrarfav() {
+eliminarfa.addEventListener('click', function () {
+    eliminarf();
+});
 
+function mostrarfav() {
     favi.innerHTML = "";
     const keys = Object.keys(localStorage);
-    const n = 0;
     fetch("https://script.google.com/macros/s/AKfycbz3EPAgZePOY21THYydS3_LH-G1ntkhneenTOSaq257QQzqevuoMXUUt6jV8EOB4JPv/exec")
 
         .then(response => response.json())
         .then(dataPre => {
-
+            console
             var data = dataPre.data;
             console.log(data);
-            for (i = 0; i < keys.length; i++) {
-                id = keys[i];
 
-                if (data[i].ID = id) 
-                {
-                    console.log("id" + id)
-                    console.log("idPRincipal" + data[i].ID)
-                    const opcion = data[i];
-                    elementos(opcion);
-                }  
-            
-    }
-});
+            for (let h = 0; h < keys.length; h++) {
+                let id = keys[h];
+                const opcion = data[id - 1];
+
+
+                const article = document.createElement('article');
+                article.classList.add('opcion');
+
+                const idopcion = opcion.ID;
+                const imagenopcion = document.createElement('img');
+                imagenopcion.src = opcion.Imagen;
+                console.log(imagenopcion);
+                article.appendChild(imagenopcion);
+
+                const nombreOpcion = document.createElement('h2');
+                nombreOpcion.setAttribute('class', 'name');
+                nombreOpcion.textContent = opcion.Nombre;
+                article.appendChild(nombreOpcion);
+
+                const precioOpcion = document.createElement('h2');
+                precioOpcion.setAttribute('class', 'precio');
+                precioOpcion.textContent = `Precio: $ ${opcion.Precio}`;
+                article.appendChild(precioOpcion);
+
+                const categoriaOpcion = document.createElement('p');
+                categoriaOpcion.textContent = `Categoría: ${opcion.Categoria}`;
+                article.appendChild(categoriaOpcion);
+
+                const descripcionOpcion = document.createElement('p');
+                descripcionOpcion.textContent = `Preparación: ${opcion.Descripcion}`;
+                article.appendChild(descripcionOpcion);
+                favi.appendChild(article);
+
+                Total(opcion.Precio);
+
+            }
+
+        });
 }
 
-
-/*
-eliminarfa.addEventListener('click', function () {
-
-    eliminarf();
-});
-*/
 function eliminarf() {
     if (window.confirm('¿Estás seguro de que deseas eliminar los favoritos?')) {
         localStorage.clear();
-        let resta = parseFloat(resultado.innerText);
-        resta -= parseFloat(precio);
-        resultado.textContent = resta.toFixed(2);
-        //mostrarfav();
-
-    } else 
-    {
+        
+        mostrarfav();
+        resultado.textContent = "0";
+    } else {
         console.log("ok");
     }
-
+    mostrarfav();
 }
-/*
-document.addEventListener('keydown', function (event) {
-    if (event.key === 'Enter') {
-        // Lógica para activar la función del botón
-        // Por ejemplo, si tienes un botón con id 'miBoton', puedes simular un clic en él
-        //buscarc()
-    }
-});
-*/
-function Total(precio)
-{
+
+function Total(precio) {
+
     const preci = precio;
-    //console.log(preci);
+    console.log(preci);
     let total = parseFloat(resultado.innerText);//obtenemos la info de resultado
     total += parseFloat(preci);//sumamos
     resultado.textContent = total.toFixed(2);//agregamos dos decimales
 
 }
-function agregarCarrito(imagen,Nombre,Categoria)
-{
 
-    const expandible=document.createElement("div");
-    const boton=document.createElement("button");
-    const verMenos=document.createElement("button");
-    const nombre=document.createTextNode(Nombre);
-    const categoria=document.createTextNode(Categoria);
-    const Imagen=document.createElement("img");
-    Imagen.src=imagen;
-    boton.textContent="ver mas";
-    verMenos.textContent="ver menos";
-
-    expandible.style.backgroundColor="orange";
-    expandible.appendChild(nombre);
-    expandible.appendChild(categoria);
-    expandible.appendChild(Imagen);
-    expandible.appendChild(verMenos);
-
-    expandible.style.display="none";
-
-    favi.appendChild(boton);
-    favi.appendChild(expandible);
-    boton.addEventListener('click',()=>{
-        expandible.style.display="block";
-        expandible.style.height="auto";
-        boton.style.display="none";
-    })
-
-    verMenos.addEventListener('click',()=>{
-        expandible.style.display="none";
-        boton.style.display="block";
-        favi.appendChild(boton);
-    })
-}
-function buscarTeclado()
-{
-    const URL="https://script.google.com/macros/s/AKfycbz3EPAgZePOY21THYydS3_LH-G1ntkhneenTOSaq257QQzqevuoMXUUt6jV8EOB4JPv/exec";
+function buscarTeclado() {
+    const URL = "https://script.google.com/macros/s/AKfycbz3EPAgZePOY21THYydS3_LH-G1ntkhneenTOSaq257QQzqevuoMXUUt6jV8EOB4JPv/exec";
     fetch(URL)
         .then(response => response.json())
         .then(dataPre => {
 
             var data = dataPre.data;
-            
-            for(let i=0;i<=14;i++)
-            {
-                const opcion=data[i];
-                if(opcion.Nombre.toLowerCase()===(foodInput.textContent.toLocaleLowerCase()))
-                {
+
+            for (let i = 0; i <= 14; i++) {
+                const opcion = data[i];
+                if (opcion.Nombre.toLowerCase() === (foodInput.textContent.toLocaleLowerCase())) {
                     encontrados(data);
                 }
             }
-            
+
         });
 }
-function encontrados(data)
-{
-    foodList.innerHTML='';
+function encontrados(data) {
+    foodList.innerHTML = '';
     data.forEach(food => {
-        const contenido=document.createElement('article');
+        const contenido = document.createElement('article');
         contenido.classList.add('Elemento');
-        contenido.innerHTML=food.Nombre;
+        contenido.innerHTML = food.Nombre;
         foodList.appendChild(contenido);
     });
 }
 
-function elementos(opcion)
-{
+function elementos(opcion) {
 
     const article = document.createElement('article');
     article.classList.add('opcion');
@@ -238,65 +170,62 @@ function elementos(opcion)
     categoriaOpcion.textContent = `Categoría: ${opcion.Categoria}`;
     article.appendChild(categoriaOpcion);
 
-            
+
     const descripcionOpcion = document.createElement('p');
     descripcionOpcion.textContent = `Preparación: ${opcion.Descripcion}`;
     article.appendChild(descripcionOpcion);
 
     const botonagregar = document.createElement('button');
     botonagregar.setAttribute('class', 'btnagregar');
-    botonagregar.innerHTML = '<i class="fa-regular fa-bookmark"></i>';
+    botonagregar.innerHTML = '<i class="fa-solid fa-plus"></i>';
 
     article.appendChild(botonagregar);
     foodList.appendChild(article);
-                        
-    //console.log(n,opcion.Nombre);
-    botonagregar.addEventListener('click',  ()=>  {
 
-        
-        Total(opcion.Precio);
+    //console.log(n,opcion.Nombre);
+    botonagregar.addEventListener('click', () => {
+
+
+
         //console.log(imagenopcion.src,nombreOpcion.textContent,opcion.Categoria);
         guardarLocal(opcion);
-        agregarCarrito(imagenopcion.src,nombreOpcion.textContent,opcion.Categoria);
-        
+
+
+
         //favoritoss(opcion);
-                            
+
     });
 }
 
-function send()
-{
-    enviar.addEventListener('click',()=>{
-        window.location.href='formulario.html';
+function send() {
+    enviar.addEventListener('click', () => {
+        window.location.href = 'formulario.html';
     })
 }
 
-function menues()
-{
-    switch (key) 
-    {
+function menues() {
+    switch (key) {
         case value:
-            
+
             break;
-    
+
         default:
             break;
     }
 }
-function entrada()
-{
-    let URL='https://script.google.com/macros/s/AKfycbz3EPAgZePOY21THYydS3_LH-G1ntkhneenTOSaq257QQzqevuoMXUUt6jV8EOB4JPv/exec';
+function entrada() {
+    let URL = 'https://script.google.com/macros/s/AKfycbz3EPAgZePOY21THYydS3_LH-G1ntkhneenTOSaq257QQzqevuoMXUUt6jV8EOB4JPv/exec';
     fetch(URL)
-        .then(response=>response.json)
+        .then(response => response.json)
         .then(dataPre)
-        let data=dataPre.data;
-        
+    let data = dataPre.data;
 
-    entradas.addEventListener('click',()=>{
+
+    entradas.addEventListener('click', () => {
 
     })
 }
-window.onload = buscarc(),send();
+window.onload = buscarc(), send();
 
 //export {pedidos};//para llevar info a otro archivo
-//window.onload = buscarc(), mostrarfav();
+window.onload = mostrarfav();
